439f1c54a561caae766662e5fec7ee1c3a97035e
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-01-27 11:06:53 -0300

    Implementação funções deslocamento left e right (#10)
    * Implementação right_shift e novas diretrizes
    
    * Implementação shift_right e fliplr
    
    * Implementação ws2812b_motion_left_shift

--- include/ws2812b_motion.c
+++ include/ws2812b_motion.c
@@ -1,11 +1,31 @@
 #include "ws2812b_motion.h"
 
 
-static void right_shift(uint8_t arr[5])
+
+static void fliplr(uint8_t *matrix) {
+    uint8_t temp;
+    temp = matrix[5];
+    matrix[5] = matrix[9]; matrix[9] = temp;
+    temp = matrix[6];
+    matrix[6] = matrix[8]; matrix[8] = temp;
+    temp = matrix[15];
+    matrix[15] = matrix[19]; matrix[19] = temp;
+    temp = matrix[16];
+    matrix[16] = matrix[18]; matrix[18] = temp;
+}
+
+static void right_shift(uint8_t *arr)
 {
     uint8_t i;
-    for(i = 4; i >= 0; i--) arr[i] = arr[i-1];
+    for(i = 4; i > 0; i--) arr[i] = arr[i-1];
     arr[0] = 0;
+}
+
+static void left_shift(uint8_t *arr)
+{
+    uint8_t i;
+    for(i = 0; i < 4; i++) arr[i] = arr[i+1];
+    arr[4] = 0;
 }
 
 void ws2812b_motion_spin(led_shape_t *shape)
@@ -25,12 +45,18 @@ void ws2812b_motion_bounce(led_shape_t *shape)
 
 void ws2812b_motion_shift_left(led_shape_t *shape)
 {
-    return;
+    uint8_t i;
+    fliplr(shape->pattern);
+    for(i = 0; i < 5; i++) left_shift(&(shape->pattern[5 * i]));
+    fliplr(shape->pattern);
 }
 
 void ws2812b_motion_shift_right(led_shape_t *shape)
 {
-    return;
+    uint8_t i;
+    fliplr(shape->pattern);
+    for(i = 0; i < 5; i++) right_shift(&(shape->pattern[5 * i]));
+    fliplr(shape->pattern);
 }
 
 void ws2812b_motion_shift_up(led_shape_t *shape)
