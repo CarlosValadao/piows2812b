aeb900e738a75f764910da993621225a0ed05fee
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-01-27 12:30:07 -0300

    Implementação ws2812b_motion_shitft_up (#11)
    * Implementação right_shift e novas diretrizes
    
    * Implementação shift_right e fliplr
    
    * Implementação ws2812b_motion_left_shift
    
    * Implementação ws2812b_motion_shift_up

--- include/ws2812b_motion.c
+++ include/ws2812b_motion.c
@@ -1,7 +1,6 @@
 #include "ws2812b_motion.h"
 
 
-
 static void fliplr(uint8_t *matrix) {
     uint8_t temp;
     temp = matrix[5];
@@ -27,6 +26,25 @@ static void left_shift(uint8_t *arr)
     for(i = 0; i < 4; i++) arr[i] = arr[i+1];
     arr[4] = 0;
 }
+
+// realiza a transposta de arr, sendo ele um array
+// de 25 posicoes (5x5)
+static void transpose(uint8_t *arr)
+{
+    uint8_t temp;
+    uint8_t i;
+    uint8_t j;
+    for(i = 0; i < 5; i++)
+    {
+        for(j=i+1; j < 5; j++)
+        {
+            temp = arr[i * 5 + j];
+            arr[i * 5 + j] = arr[j * 5 + i];
+            arr[j * 5 + i] = temp;
+        }
+    }
+}
+
 
 void ws2812b_motion_spin(led_shape_t *shape)
 {
@@ -61,7 +79,12 @@ void ws2812b_motion_shift_right(led_shape_t *shape)
 
 void ws2812b_motion_shift_up(led_shape_t *shape)
 {
-    return;
+    uint8_t i;
+    fliplr(shape->pattern);
+    transpose(shape->pattern);
+    for(i = 0; i < 5; i++) left_shift(&(shape->pattern[5 * i]));
+    transpose(shape->pattern);
+    fliplr(shape->pattern);
 }
 
 void ws2812b_motion_shift_down(led_shape_t *shape)
