95c0ff531f580c36af2925897385d418816d7bd6
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-01-28 23:36:33 -0500

    Integração final

--- pio_matrix.c
+++ pio_matrix.c
@@ -21,10 +21,10 @@
 //pino de saída
 #define OUT_PIN 7
 
+#define set_bootsel_mode() reset_usb_boot(0, 0)
 
 //botão de interupção
-const uint button_0 = 5;
-const uint button_1 = 6;
+const uint BUTTON_A = 5;
 
 
 //rotina da interrupção
@@ -35,7 +35,6 @@ static void gpio_irq_handler(uint gpio, uint32_t events){
 }
 
 
-//função principal
 int main()
 {
     PIO pio = pio0; 
@@ -44,62 +43,108 @@ int main()
     uint32_t valor_led;
     uint8_t *map = NULL;
     uint8_t shifted_glyph[25];
-
     ws2812b_t *ws = NULL;
+    char pressed_key = '\0';
+    uint8_t temp_glyph[25] = {0};
 
-    //coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
+    uint columns[4] = {21, 20, 19, 18};
+    uint rows[4] = {16, 27, 26, 22};
+    char KEY_MAP[16] = {
+        '1', '2', '3', 'A',
+        '4', '5', '6', 'B',
+        '7', '8', '9', 'C',
+        '*', '0', '#', 'D'
+    };
+
+    keyboard4x4 *keyboard;
+
+    keyboard = keyboard_init(columns, rows, KEY_MAP);
+
+    buzzer_init_default();
+
     ok = set_sys_clock_khz(128000, false);
-
-    // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
     stdio_init_all();
 
     printf("iniciando a transmissão PIO");
     if (ok) printf("clock set to %ld\n", clock_get_hz(clk_sys));
 
-    //configurações da PIO
-    // uint offset = pio_add_program(pio, &pio_matrix_program);
-    // uint sm = pio_claim_unused_sm(pio, true);
-    // pio_matrix_program_init(pio, sm, offset, OUT_PIN);
     ws = init_ws2812b(pio, OUT_PIN);
 
-    //inicializar o botão de interrupção - GPIO5
-    gpio_init(button_0);
-    gpio_set_dir(button_0, GPIO_IN);
-    gpio_pull_up(button_0);
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
 
-    gpio_init(button_1);
-    gpio_set_dir(button_1, GPIO_IN);
-    gpio_pull_up(button_1);
-
+    uint8_t k;
     //interrupção da gpio habilitada
-    gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, 1, & gpio_irq_handler);
-    buzzer_init_default();
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, 1, & gpio_irq_handler);
     ws2812b_turn_off_all(ws);
-    uint k;
+    printf("Iniciando o programa...\n");
     while (true) {
-        //buzzer_beep_default(1000, 300);
-        //send_ws2812b_data(ws->pio, ws->state_machine_id, 68280);
-        //ws2812b_draw(ws, &shape);
-        /*for (int i = 0; i < 25; i++) {
-            uint8_t physical_index = map[i]; // Usa o mapeamento
-            if (pattern[physical_index] == 1) {
-                send_ws2812b_data(ws->pio, ws->state_machine_id, 0xFF0000); // Exemplo: vermelho
-            } else {
-                send_ws2812b_data(ws->pio, ws->state_machine_id, 0x000000); // Desligar LED
+        pressed_key = keyboard_get_pressed_key(keyboard);
+        if(pressed_key != '\0')
+        {
+            switch(pressed_key)
+            {
+                case '1':
+                    ws2812b_motion_slide_left(ws, ONE_GLYPH, GREEN, 100);
+                    break;
+                case '2':
+                    ws2812b_motion_slide_right(ws, TWO_GLYPH, RED, 100);
+                    break;
+                case '3':
+                    ws2812b_motion_slide_up(ws, THREE_GLYPH, YELLOW, 100);
+                    break;
+                case '4':
+                    ws2812b_motion_slide_down(ws, FOUR_GLYPH, PURPLE, 100);
+                    break;
+                case '5':
+                    ws2812b_motion_spin(ws, BLUE_MARINE, 10);
+                    break;
+                case '6':
+                    ws2812b_motion_pulse(ws, temp_glyph);
+                    for(k = 0; k < 5; k++)
+                    {
+                        buzzer_beep_default(500, 2000);
+                        sleep_ms(700);
+                    }
+                    break;
+                case '7':
+                    ws2812b_motion_left_colision(ws, ONE_GLYPH, PURPLE, 100);
+                    break;
+                case '8':
+                    rotate_clockwise_90_deg_glyph(ws, TWO_GLYPH, YELLOW, 100);
+                    break;
+                case '9':
+                    rotate_clockwise_90_deg_glyph(ws, TWO_GLYPH, YELLOW, 100);
+                    break;
+                case 'A':
+                    ws2812b_turn_off_all(ws);
+                    break;
+                case 'B':
+                    ws2812b_draw(ws, ALL_LEDS_ON, BLUE, 100);
+                    sleep_ms(100);
+                    break;
+                case 'C':
+                    ws2812b_draw(ws, ALL_LEDS_ON,  RED, 80);
+                    sleep_ms(100);
+                    break;
+                case 'D':
+                    ws2812b_draw(ws, ALL_LEDS_ON, GREEN, 50);
+                    sleep_ms(100);
+                    break;
+                case '#':
+                    ws2812b_draw(ws, ALL_LEDS_ON, WHITE, 20);
+                    sleep_ms(100);
+                    break;
+                case '*':
+                    set_bootsel_mode();
+                    break;
+                case '0':
+                    ws2812b_motion_slide_left(ws, ALL_LEDS_ON, WHITE, 100);
+                    break;
             }
+            printf("%c\n", pressed_key);
         }
-        */
-       for(k=0;k<10;k++)
-       {
-        ws2812b_draw(ws, NUMERIC_GLYPHS[k], PURPLE, 1);
-        sleep_ms(1500);
-        ws2812b_turn_off_all(ws);
-        ws2812b_motion_shift_left(NUMERIC_GLYPHS[k], shifted_glyph);
-        ws2812b_draw(ws, shifted_glyph, GREEN, 1);
-        sleep_ms(1500);
-        ws2812b_turn_off_all(ws);
-        sleep_ms(50);
-       }
-        printf("\nfrequeência de clock %ld\r\n", clock_get_hz(clk_sys));
+    
     }
 }
